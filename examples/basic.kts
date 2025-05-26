6f837b0030c742a95182d41ed8f935c1c15ab39d
gilesjb <gilesjb@gmail.com>
2025-05-26 08:54:35 -0700

    Added Java version of basic build script

--- examples/basic.kts
+++ examples/basic.kts
@@ -1,17 +1,17 @@
 #!/usr/bin/env kotlin -Xjvm-default=all -cp .jam-classes
 
-interface BasicProject : JavaProject {
+interface SimpleKts : JavaProject {
 
     fun mainSources() = sourceFiles("main/**.java")
 
     fun mainClasses() = javac("classes/main", mainSources())
 
-    fun docs() = javadoc("docs", "-sourcepath", classpath(mainSources()), "-subpackages", "")
-    
+    fun docs() = javadoc("docs", "-sourcepath", classpath(mainSources()), "-subpackages", "", "-quiet")
+
     fun all() {
         docs()
         mainClasses()
     }
 }
 
-Project.run(BasicProject::class.java, BasicProject::all, args)
+Project.run(SimpleKts::class.java, SimpleKts::all, args)
