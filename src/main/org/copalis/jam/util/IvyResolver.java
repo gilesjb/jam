f2390ca03db2fd0079406018cf1be0f43111f6c2
gilesjb <gilesjb@gmail.com>
2026-03-25 23:25:27 -0700

    Refactored IvyResolver

--- src/main/org/copalis/jam/util/IvyResolver.java
+++ src/main/org/copalis/jam/util/IvyResolver.java
@@ -1,10 +1,8 @@
 package org.copalis.jam.util;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
-import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -26,6 +24,10 @@ public record IvyResolver(String url, String cacheDir, File settingsFile) implem
      */
     public static final String VER2_5_1_URL = "https://repo1.maven.org/maven2/org/apache/ivy/ivy/2.5.1/ivy-2.5.1.jar";
 
+    /**
+     * Gets a reference to a local copy of the Ivy jar file
+     * @return the file path
+     */
     public Path ivyJar() {
         Path path = Path.of(cacheDir, url.substring(url.lastIndexOf('/') + 1));
         if (!path.toFile().exists()) {
@@ -36,43 +38,35 @@ public record IvyResolver(String url, String cacheDir, File settingsFile) implem
     }
 
     @Override public Stream<Path> resolve(String... dependencies) {
-        try {
-            File ivyFile = File.createTempFile("ivy-", ".xml");
-            ivyFile.deleteOnExit();
+        StringBuilder xml = new StringBuilder();
+        xml.append("<ivy-module version='2.0' xmlns:m='http://ant.apache.org/ivy/maven'>");
+        xml.append("<info organisation='org' module='module'/>");
+        xml.append("<dependencies defaultconf='*->default'>");
 
-            try (Writer out = new FileWriter(ivyFile)) {
-                out.write("<ivy-module version='2.0' xmlns:m='http://ant.apache.org/ivy/maven'>");
-                out.write("<info organisation='org' module='module'/>");
-                out.write("<dependencies defaultconf='*->default'>");
-
-                for (String ident : dependencies) {
-                    int pound = ident.indexOf('#');
-                    if (pound >= 0) {
-                        String[] parts = ident.substring(0, pound).split(":");
-                        out.write(String.format("<dependency org='%s' name='%s' rev='%s' m:classifier='%s'/>",
-                                parts[0], parts[parts.length - 2], parts[parts.length - 1], ident.substring(pound + 1)));
-                    } else {
-                        String[] parts = ident.split(":");
-                        out.write(String.format("<dependency org='%s' name='%s' rev='%s' />",
-                                parts[0], parts[parts.length - 2], parts[parts.length - 1]));
-
-                    }
-                }
-                out.write("</dependencies></ivy-module>");
+        for (String ident : dependencies) {
+            int pound = ident.indexOf('#');
+            if (pound >= 0) {
+                String[] parts = ident.substring(0, pound).split(":");
+                xml.append(String.format("<dependency org='%s' name='%s' rev='%s' m:classifier='%s'/>",
+                        parts[0], parts[parts.length - 2], parts[parts.length - 1], ident.substring(pound + 1)));
+            } else {
+                String[] parts = ident.split(":");
+                xml.append(String.format("<dependency org='%s' name='%s' rev='%s' />",
+                        parts[0], parts[parts.length - 2], parts[parts.length - 1]));
             }
-
-            File cacheFile = runIvy(ivyFile, settingsFile);
-
-            return Stream.of(Files.readString(cacheFile.toPath()).trim().split(":"))
-                    .map(Path::of);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        xml.append("</dependencies></ivy-module>");
+        return download(tempFile("ivy-", ".xml", xml), settingsFile);
     }
 
-    public File runIvy(File ivyFile, File settingsFile) throws IOException {
-        File cacheFile = File.createTempFile("classpath-", null);
-        cacheFile.deleteOnExit();
+    /**
+     * Executes Ivy
+     * @param ivyFile an optional ivy.xml file
+     * @param settingsFile an optional settings.xml file
+     * @return a stream of Path objects referencing the resolved dependencies
+     */
+    public Stream<Path> download(File ivyFile, File settingsFile) {
+        File cacheFile = tempFile("classpath-", null, null);
 
         Args cmd = Args.of("java", "-jar", ivyJar().toString(),
                 "-cache", cacheDir,
@@ -86,6 +80,24 @@ public record IvyResolver(String url, String cacheDir, File settingsFile) implem
             cmd.and("-settings", settingsFile.toString());
         }
         cmd.run();
-        return cacheFile;
+        try {
+            return Stream.of(Files.readString(cacheFile.toPath()).trim().split(":")).map(Path::of);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private File tempFile(String prefix, String suffix, CharSequence content) {
+        File tmp;
+        try {
+            tmp = File.createTempFile(prefix, suffix);
+            tmp.deleteOnExit();
+            if (Objects.nonNull(content)) {
+                Files.writeString(tmp.toPath(), content);
+            }
+            return tmp;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
