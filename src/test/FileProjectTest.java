edc3956baa540b25d5d466abcf14d25aa8ab2717
gilesjb <gilesjb@gmail.com>
2026-08-16 00:06:25 -0700

    FileProjectTest cleanup
    minor tidy-up of disabled test

--- src/test/FileProjectTest.java
+++ src/test/FileProjectTest.java
@@ -1,45 +1,55 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 class FileProjectTest {
-    static List<String> log;
 
-    public interface TestProject extends FileProject {
+    public interface FileReader extends FileProject {
         default String fileContents() {
             log.add("reading file");
             return read("test/file.txt");
         }
-
-        default void testFile() {
-            assertEquals("hello", fileContents());
-        }
     }
 
+    static List<String> log;
+    final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+
     @BeforeEach public void setup() {
-        Project.run(TestProject.class, TestProject::clean, new String[] {});
+        bytes.reset();
+        System.setOut(new PrintStream(bytes));
+        Project.run(FileReader.class, FileReader::clean, new String[] {});
         log = new LinkedList<>();
     }
 
-//    @Test
-    void testFileModification() throws URISyntaxException, IOException {
-//        Project.run(TestProject.class, TestProject::testFile, new String[] {});
-//        assertEquals(1, log.size());
-//
-//        File file = new File("src/test/file.txt");
-//
-//        long lastModified = file.lastModified();
-//        file.setLastModified(lastModified + 1);
-//        long nowModified = file.lastModified();
-//
-//        assertNotEquals(lastModified, nowModified);
-//
-//        Project.run(TestProject.class, TestProject::fileContents, new String[] {});
-//        assertEquals(2, log.size());
+    @AfterEach public void cleanup() {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+    }
+
+//    @Test // currently disabled because it breaks Jypeter
+    public void testFileModification() throws URISyntaxException, IOException {
+        Project.run(FileReader.class, FileReader::fileContents, new String[] {});
+        assertEquals(1, log.size());
+
+        File file = new File("src/test/file.txt");
+
+        long lastModified = file.lastModified();
+        file.setLastModified(lastModified + 1);
+        long nowModified = file.lastModified();
+
+        assertNotEquals(lastModified, nowModified);
+
+        Project.run(FileReader.class, FileReader::fileContents, new String[] {});
+        assertEquals(2, log.size());
     }
 }
